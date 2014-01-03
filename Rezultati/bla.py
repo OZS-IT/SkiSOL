cd5aa893fa8ef7c5df8c05092f5f7a78b4730ffa
andrejborstnik <andrej-borstnik@hotmail.com>
2014-01-03 11:43:20 +0100

    Tisto je bil SOL, ne SKI SOL, zato brišem.

--- Rezultati/bla.py
+++
@@ -1,7 +0,0 @@
-with open("SOL3a.csv",'r+',encoding="utf-8") as f:
-    with open("SOL3.csv",'r+',encoding="utf-8") as g:
-        for i in f:
-            for j in i:
-                if j!="\"":
-                    g.write(j)
-        
