1a5cf382130607a33b4d51e9627198395f79bb70
andrejborstnik <andrej-borstnik@hotmail.com>
2018-01-03 21:22:12 +0100

    SkiSOL 1 in 2.

--- sol.py
+++ sol.py
@@ -45,7 +45,7 @@ for st_lige in range(1,12):
         st_tekem+=1
         vCsv(stanjeLige,st_tekem,kat,tek)
 if path.isfile('./Resna stanja/SSOL'+str(st_tekem)+'.csv'):
-    g=open('skisol_2016.csv','w',encoding='utf-8')
+    g=open('skisol_2018.csv','w',encoding='utf-8')
     with open('./Resna stanja/SSOL'+str(st_tekem)+'.csv','r',encoding='utf-8') as f:
         for i in f.readlines():
             g.write(i)
